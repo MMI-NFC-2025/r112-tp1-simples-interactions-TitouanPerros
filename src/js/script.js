78150f454c7cbb61f3e7c7103c6d5cbe75652093
TitouanPerros <titouan.perros02@edu.univ-fcomte.fr>
2025-11-07 08:59:58 +0100

    exo1 +exo2

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 // TODO
+//exo1
+const elm = document.querySelector("#a-metrre-en-rouge").style.color="red"
+//exo2
+const elm = document.querySelector("#a-mettre-en-rouge")
+const button = document.querySelector("#en-rouge-suite-a-click")
+button.addEventListener("click",function(){elm.style.color ="red"})
+//exo3
+
+
+

0d6bdaf78bcddc7a2d71a4adc411db7fd1b08de5
TitouanPerros <titouan.perros02@edu.univ-fcomte.fr>
2025-11-07 09:45:32 +0100

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 // TODO
 //exo1
-const elm = document.querySelector("#a-metrre-en-rouge").style.color="red";
-//exo2
 const elm = document.querySelector("#a-mettre-en-rouge");
+elm.style.color="red";
+//exo2
 const button = document.querySelector("#en-rouge-suite-a-click");
 button.addEventListener("click",function(){elm.style.color ="red"});
 //exo3

4741a729b514cab4c36733351ca9c0e4e1ed5631
TitouanPerros <titouan.perros02@edu.univ-fcomte.fr>
2025-11-07 09:17:21 +0100

    exo 3

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,19 @@
 // TODO
 //exo1
-const elm = document.querySelector("#a-metrre-en-rouge").style.color="red"
+const elm = document.querySelector("#a-metrre-en-rouge").style.color="red";
 //exo2
-const elm = document.querySelector("#a-mettre-en-rouge")
-const button = document.querySelector("#en-rouge-suite-a-click")
-button.addEventListener("click",function(){elm.style.color ="red"})
+const elm = document.querySelector("#a-mettre-en-rouge");
+const button = document.querySelector("#en-rouge-suite-a-click");
+button.addEventListener("click",function(){elm.style.color ="red"});
 //exo3
+const coll = document.querySelectorAll("h2");
 
+  coll.forEach((elm) => {
+    elm.addEventListener("click", (evt) => {
+      console.log("Événement :", evt.type);
+      console.log("Ciblé :", evt.target.textContent);
+      evt.target.style.color = "red";
+    });
+  });
 
 
